d4ec22637dd868dd7164629882ffb4caeb42b94e
Josh Vaysman <vaysman@chapman.edu>
2025-04-05 12:32:48 -0700

    git commit

--- CPSC 408/Assignment_05/main.py
+++ CPSC 408/Assignment_05/main.py
@@ -12,4 +12,43 @@ mydb = mysql.connector.connect(
     )
 print(mydb)
 
+mycursor = mydb.cursor()
+
+mycursor.execute("USE rideShare")
+
+# mycursor.execute("""
+#        CREATE TABLE rides (
+#             rideID INT NOT NULL PRIMARY KEY,
+#             riderID INT REFERENCES rider(riderID),
+#             driverID INT REFERENCES driver(driverID),
+#             startSpot VARCHAR(40),
+#             endSpot VARCHAR(40),
+#             Completed TINYINT(1)
+#         );""")
+
+# mycursor.execute("""
+#                 ALTER TABLE rider
+#                 ADD CONSTRAINT lastrideID FOREIGN KEY (lastrideID) REFERENCES rides(rideID);
+#                  """)
+
+# class Driver:
+#     def __init__(self, driverID):
+#         self.driverID = driverID
+
+#     def viewRating(self):
+#         mycursor.execute("SELECT Rating FROM Driver WHERE driverID = " + self.driverID)
+
+#     def viewRides(self):
+#         mycursor.execute("""
+#         SELECT RideID
+#         FROM Ride WHERE driverID = """ + self.driverID)
+
+#     # def setStatus(pick: bool):
+#     #     if type(pick) is not bool:
+#     #         raise ValueError("pick must be a boolean value (True or False)")
+#     #     mycursor.execute("""
+#     #     ALTER TABLE Driver
+#     #     MODIFY COLUMN WHERE 
+#     #     """)
+
 
