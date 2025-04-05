6f9b77b6a1947304b19eff87edb3ac6db9242f80
Josh Vaysman <vaysman@chapman.edu>
2025-04-05 11:22:09 -0700

    git commit

---
+++ CPSC 408/Assignment_05/main.py
@@ -0,0 +1,15 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+    host = os.getenv('MYSQL_HOST'),
+    user = os.getenv('MYSQL_USERNAME'),
+    password = os.getenv('MYSQL_PASSWORD'),
+    auth_plugin = 'mysql_native_password'
+    )
+print(mydb)
+mydb.close()
+
